659a5ba514b143a307861b14d35bcea88afafd29
watchagame <evancspellman@gmail.com>
2022-08-12 23:33:00 -0700

    no title

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,8 +1,8 @@
 export const getGlobalData = () => {
-  const name = process.env.BLOG_NAME ? decodeURI(process.env.BLOG_NAME) : '';
+  const name = process.env.BLOG_NAME ? decodeURI(process.env.BLOG_NAME) : ' ';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : '';
+    : ' ';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
