bbe6df1bf86ca31ebac05e9e77f57c71958ef372
watchagame <evancspellman@gmail.com>
2022-08-18 18:17:30 -0700

    maybe image fix

--- components/SEO.js
+++ components/SEO.js
@@ -10,7 +10,10 @@ export default function SEO({ title, description, hi }) {
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={hi} />
+      <meta
+        name="twitter:image"
+        content="https://www.okaybasketball.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fhi.0c4f657d.png"
+      />
     </Head>
   );
 }
