debb7447aa99b108f8391ccdf21ddc49dd231ab7
watchagame <evancspellman@gmail.com>
2022-08-18 21:43:21 -0700

    more work on default image for twitter card

--- components/SEO.js
+++ components/SEO.js
@@ -7,11 +7,12 @@ export default function SEO({ title, description, image }) {
       <meta name="description" content={description} />
       <meta property="og:image" content={image} />
       <meta property="og:title" content={title} />
-      <meta property="twitter:card" content="summary_large_image" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:name" content="okaybasketball" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta
-        property="twitter:image"
+        name="twitter:image"
         content="https://i.postimg.cc/zDL7Rj01/logotest3.png"
       />
     </Head>
