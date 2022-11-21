00c4d28a1d92c1bc54ae128ad1a0845e31d57ba9
watchagame <evancspellman@gmail.com>
2022-11-21 14:49:56 -0800

    trying again with nextjs update

--- pages/playerPhoto.js
+++ pages/playerPhoto.js
@@ -1,28 +1,29 @@
 import React, { useState } from 'react';
 import './styles.css';
 import Image from 'next/image';
+import { PRERENDER_REVALIDATE_ONLY_GENERATED_HEADER } from 'next/dist/server/api-utils';
 
-function getImageUrls(id) {
+function getImageUrls(idx) {
   if (id === undefined) {
     return [];
   }
   return [
-    `https://s3.us-west-2.amazonaws.com/images.playswoops.com/${id}_no_bg_375x375.png`,
-    `https://s3.us-west-2.amazonaws.com/images.playswoops.com/${id}_no_bg_750x750.png`,
-    `https://s3.us-west-2.amazonaws.com/images.playswoops.com/${id}_no_bg_1500x1500.png`,
+    `https://s3.us-west-2.amazonaws.com/images.playswoops.com/${idx}_no_bg_375x375.png`,
+    `https://s3.us-west-2.amazonaws.com/images.playswoops.com/${idx}_no_bg_750x750.png`,
+    `https://s3.us-west-2.amazonaws.com/images.playswoops.com/${idx}_no_bg_1500x1500.png`,
   ];
 }
 
 export default function App() {
-  const [id, setId] = useState();
+  const [idx, setId] = useState();
 
-  const imageUrls = getImageUrls(id);
+  const imageUrls = getImageUrls(idx);
   return (
-    <div className="App" key={Image.src}>
+    <div className="App" key={player.id}>
       <input
         placeholder="Enter player id"
         type="number"
-        value={id}
+        value={idx}
         onChange={(e) => setId(e.target.value)}
       />
       <div className="image-container">
